672e0f7d8dc95b68a54f144b744cecfbe4381132
Tess647 <thanni_taslihah@yahoo.com>
2023-10-08 19:04:51 +0000

    task 1

--- shell.h
+++ shell.h
@@ -2,5 +2,12 @@
 #define SHELL_H
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-#endif SHELL_H
+void execute(char **argv);
+
+#endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,1 +1,55 @@
 #include "shell.h"
+
+/**
+ * main - main function for the shell program
+ *
+ * Return: 0 Always successful otherwise -1
+ */
+
+int main(void)
+{
+	char *cmdline = NULL, *cmdline_copy = NULL, *token = NULL;
+	size_t n = 0;
+	char **argv = NULL, *delim = " \n";
+	int i = 0, argc = 0;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		/* read from the user */
+		if (getline(&cmdline, &n, stdin) == -1)
+		{
+			printf("\n");
+			return (-1);
+		}
+
+		cmdline_copy = strdup(cmdline);
+
+		token = strtok(cmdline, delim);
+		while (token)
+		{
+			token = strtok(NULL, delim);
+			argc++;
+		}
+
+		argv = malloc(sizeof(char *) * argc);
+
+		token = strtok(cmdline_copy, delim);
+		i = 0;
+
+		while (token)
+		{
+			argv[i] = token;
+			token = strtok(NULL, delim);
+			i++;
+		}
+		argv[i] = NULL;
+
+		/* execute cmmand */
+		execute(argv);
+
+	}
+	free(cmdline), free(cmdline_copy), free(argv);
+
+	return (0);
+}
