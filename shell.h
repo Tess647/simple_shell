3904265e6525c89c6ca057c6d6c0f9ae8dbb2851
Tess647 <thanni_taslihah@yahoo.com>
2023-10-11 11:57:12 +0000

    added struct to header file

--- shell.h
+++ shell.h
@@ -8,9 +8,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+int _strlen(char *str);
 void _puts(char *c);
-char _putchar(char c);
+int _putchar(char c);
 char *_strdup(const char *s);
+
 void execute(char **argv);
+
+extern char **environ;
+
+/**
+ * struct path_list - Linked list containing PATH directories
+ * @directory: dir in path
+ * @pointer: pointer to next node
+ */
+typedef struct path_list
+{
+	char *dir;
+	struct path_list *pointer;
+} path_list;
 
 #endif
