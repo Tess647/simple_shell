1e9bcaa96cd5c012347f36cbb6701d8eb132c997
Esther Olaboade <olaboadeo@gmail.com>
2023-10-21 01:03:04 +0100

    betty proofing main function

--- simple_shell.c
+++ simple_shell.c
@@ -8,28 +8,23 @@
 
 int main(void)
 {
-	char *cmdline = NULL;
 	size_t n = 0;
-	char **argv = NULL, *delim = " \n", *path, *pathname;
+	char **argv = NULL, *delim = " \n", *path, *pathname, *cmdline = NULL;
 	int len = 0;
 	path_list *head = '\0';
 	void (*func)(char **);
 
 	signal(SIGINT, signal_handler);
-
 	while (len != EOF)
 	{
 		_isatty();
 		len = getline(&cmdline, &n, stdin);
 		_EOF(len, cmdline);
-
 		argv = split_command_line(cmdline, delim);
 		if (!argv || !argv[0])
 			execute(argv);
-
 		else
 		{
-			/* Parse PATH and create a linked list of directories */
 			path = _getenv("PATH");
 			head = add_nodes(path);
 			pathname = _whichpath(argv[0], head);
